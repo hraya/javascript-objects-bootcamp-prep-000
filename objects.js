3542efce47dac5e159c03be3b2fd5588a7f99da0
Jr <hraya@lssu.edu>
2019-07-27 03:28:14 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -8,6 +8,6 @@ function updatePlayList(playlist, artist, song){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName
+  delete playlist[artistName]
   return playlist
 }
